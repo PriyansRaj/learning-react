7daeaac86532a6a1d041bcb0577b9a139669f6e6
= <=>
2025-09-04 14:52:39 +0530

    Update RemValue function

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -1,29 +1,31 @@
 import { useState } from "react";
-function App(){
-  const [value,setvalue] = useState(0);
+function App() {
+  const [value, setvalue] = useState(0);
 
-  const addValue = () =>{
+  const addValue = () => {
     // console.log("Value added ",Math.random()*10);
     // counter++;
     // console.log(counter);
-    let temp = value
-    temp+=1;
+    let temp = value;
+    temp += 1;
     setvalue(temp);
-    
-    
-  }
+  };
 
-  function RemValue(){
-    let temp =value;
-temp-=1;
-setvalue(temp);
+  function RemValue() {
+    let temp = value;
+    temp -= 1;
+    if(temp<0){
+      temp =0
+    }
+    setvalue(temp);
   }
-  return <>
-  <h1>Counter Value: {value}</h1>
-  <button onClick={addValue}>Increase</button> <br />
-  <button onClick={RemValue}>Decrease</button>
-  </>
+  return (
+    <>
+      <h1>Counter Value: {value}</h1>
+      <button onClick={addValue}>Increase</button> <br />
+      <button onClick={RemValue}>Decrease</button>
+    </>
+  );
 }
 
-
-export default  App;
+export default App;
